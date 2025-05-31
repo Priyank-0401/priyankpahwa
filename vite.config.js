4d6f27e9e82835e8482a37a6a7919df89141d3ff
Priyank Pahwa <pahwapriyank71@gmail.com>
2025-05-31 22:02:32 +0530

    Publish

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/priyankpahwa/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  publicDir: 'public'
 })
