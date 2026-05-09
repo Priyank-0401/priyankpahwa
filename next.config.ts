5a834c1247f19faa6abb01df71a4b2d9cc0819c4
Priyank Pahwa <pahwapriyank71@gmail.com>
2026-05-09 11:17:01 +0530

    Revert "Add assetPrefix and update workflow to create basePath directory structure"
    This reverts commit 66c0919f2ac18613528e151c3b5e25e0ba5b8bc6.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/priyankpahwa",
-  assetPrefix: "/priyankpahwa",
   images: {
     unoptimized: true,
   },
